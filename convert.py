617cc0f3f0519d250352bbc91471ebfe4e614a3b
Harsh Patel <hapatel2@cisco.com>
2020-05-06 15:21:25 -0700

    Added goa generator version

--- convert.py
+++ convert.py
@@ -1,6 +1,6 @@
 import ast
 
-with open("disneyland-randomizer", "r") as fd:
+with open("goa-random", "r") as fd:
     lines = fd.readlines()
     for line in lines:
         print('"{}",'.format(line.strip()))
